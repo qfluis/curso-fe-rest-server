6debc9bd59fa46730114ff87e88f31fbcf3c66c2
Luis Quevedo <qfluis@gmail.com>
2022-05-01 17:57:11 +0200

    Añadido middleware de validación datos

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -1,7 +1,7 @@
 const { response, request } = require('express');
 const bcryptjs = require('bcryptjs');
 const Usuario = require('../models/usuario');
-const { validationResult } = require('express-validator');
+
 
 const usuariosGet = (req, res = response) => {
 
@@ -26,11 +26,6 @@ const usuariosPut = (req = request, res = response) => {
 }
 
 const usuariosPost = async (req, res = response) => {
-
-    const errors = validationResult(req);
-    if ( !errors.isEmpty()){
-        return res.status(400).json(errors);
-    }
 
     const { nombre, correo, password, rol } = req.body;
     const usuario = new Usuario({ nombre, correo, password, rol });
--- routes/usuarios.js
+++ routes/usuarios.js
@@ -1,6 +1,8 @@
 const { Router } = require('express');
 const { check } = require('express-validator');
 
+const { validarCampos } = require('../middlewares/validar-campos');
+
 const { usuariosGet, usuariosPut, usuariosPost, usuariosDelete, usuariosPatch } = require ('../controllers/usuarios')
 
 const router = Router();
@@ -10,7 +12,11 @@ router.get('/', usuariosGet);
 router.put('/:id', usuariosPut);
 
 router.post('/', [
-    check('correo', 'El correo no es válido').isEmail()
+    check('correo', 'El correo no es válido').isEmail(),
+    check('nombre', 'El nombre es obligatorio').not().isEmpty(),
+    check('password', 'El password debe de tener más de 6 letras').isLength({ min: 6 }),
+    check('rol', 'No es un rol permitido').isIn(['ADMIN_ROLE', 'USER_ROLE']),
+    validarCampos
 ] , usuariosPost);
 
 router.delete('/', usuariosDelete);
